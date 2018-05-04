fd643146b292d2bd23057d2b5e038d54179e0433
Pollux <mbuelow42@outlook.de>
2018-05-04 12:03:58 +0200

    initial branch commit

---
+++ Server/Server/src/network/main - Copy.java
@@ -0,0 +1,21 @@
+package network;
+
+import java.net.*;
+import java.io.*;
+
+public class main {
+
+
+	public void main(String[] args) {
+		
+		// asynchroner listener
+		AsynServerListener server = new AsynServerListener(Integer.parseInt(args[0]));
+		server.start();
+		
+		// interaktive Konsole
+		// TODO implement
+	}
+	
+
+}
+
